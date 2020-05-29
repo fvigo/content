ba89cd7b54db48dc818a3938b448be1b2c633fad
Francesco Vigo <fvigo@paloaltonetworks.com>
2020-05-29 13:20:17 +0200

    Initial test

---
+++ Packs/EventProcessing/Integrations/LongRunningTest/LongRunningTest.py
@@ -0,0 +1,68 @@
+import json
+import dateparser
+
+import demistomock as demisto
+from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
+from CommonServerUserPython import *  # noqa: E402 lgtm [py/polluting-import]
+
+
+# CONSTANTS
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+
+class Client(BaseClient):
+    """
+    Client will implement the service API, and should not contain any Demisto logic.
+    Should only do requests and return data.
+    """
+
+    def say_hello(self, name):
+        return f'Hello {name}'
+
+
+def test_module(client):
+    return 'ok'
+
+def say_hello_command(client, args):
+    return client.say_hello(args.get('name', 'NoName'))
+
+def main():
+    """
+        PARSE AND VALIDATE INTEGRATION PARAMS
+    """
+    username = demisto.params().get('credentials').get('identifier')
+    password = demisto.params().get('credentials').get('password')
+
+    # get the service API url
+    base_url = urljoin(demisto.params()['url'], '/api/v1/suffix')
+
+    verify_certificate = not demisto.params().get('insecure', False)
+
+    proxy = demisto.params().get('proxy', False)
+
+    (f'Command being called is {demisto.command()}')
+    try:
+        client = Client(
+            base_url=base_url,
+            verify=verify_certificate,
+            auth=(username, password),
+            proxy=proxy)
+
+        if demisto.command() == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            result = test_module(client)
+            demisto.results(result)
+
+        elif demisto.command() == 'long-running-execution':
+            pass
+
+        elif demisto.command() == 'longrunningtest-hup':
+            return_outputs(*say_hello_command(client, demisto.args()))
+
+    # Log exceptions
+    except Exception as e:
+        return_error(f'Failed to execute {demisto.command()} command. Error: {str(e)}')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
