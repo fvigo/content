843d379fac0a03f100c84df3c101fd630f72730e
Francesco Vigo <fvigo@paloaltonetworks.com>
2020-06-08 16:28:33 +0200

    Long Running test going on

--- Packs/EventProcessing/Integrations/LongRunningTest/LongRunningTest.py
+++ Packs/EventProcessing/Integrations/LongRunningTest/LongRunningTest.py
@@ -1,10 +1,15 @@
 import json
 import dateparser
 import time
+import socket
+import threading
 
+from typing import Any
 import demistomock as demisto
 from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
 from CommonServerUserPython import *  # noqa: E402 lgtm [py/polluting-import]
+
+bind_ip = '0.0.0.0'
 
 class Client(BaseClient):
     """
@@ -31,12 +36,64 @@ def update_cnt(client, args):
     demisto.log(f'now cnt is: {cnt}')    
     return client.say_hello(str(cnt)), None, None
 
+def try_parse_integer(int_to_parse: Any, err_msg: str) -> int:
+    """
+    Tries to parse an integer, and if fails will throw DemistoException with given err_msg
+    """
+    try:
+        res = int(int_to_parse)
+    except (TypeError, ValueError):
+        raise DemistoException(err_msg)
+    return res
+
+def get_params_port(params: dict = demisto.params()) -> int:
+    """
+    Gets port from the integration parameters
+    """
+    port_mapping: str = params.get('longRunningPort', '')
+    err_msg: str
+    port: int
+    if port_mapping:
+        err_msg = f'Listen Port must be an integer. {port_mapping} is not valid.'
+        if ':' in port_mapping:
+            port = try_parse_integer(port_mapping.split(':')[1], err_msg)
+        else:
+            port = try_parse_integer(port_mapping, err_msg)
+    else:
+        raise ValueError('Please provide a Listen Port.')
+    return port
+
+def handle_client_connection(client_socket):
+    client_socket.send(b'Welcome\n')
+    request = client_socket.recv(1024)
+    demisto.debug(f'Long Running: Received {request}')
+    client_socket.send(b'ACK!\n')
+    ctx = demisto.getIntegrationContext()
+    cnt = ctx.get('cnt', 0)
+    demisto.debug(f'ctx: {json.dumps(ctx)}')
+    demisto.debug(f'cnt: {cnt}')  
+    new_cnt = cnt+1
+    demisto.setIntegrationContext({'cnt': new_cnt, 'msg': str(request)})
+    client_socket.close()
+
 def long_running_loop():
-    while True:
-        cnt = demisto.getIntegrationContext().get('cnt', 0)
-        demisto.updateModuleHealth(f'hi: {cnt}')
-        demisto.setIntegrationContext({'cnt': cnt+1})
-        time.sleep(10)
+    try:
+        bind_port = get_params_port()
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((bind_ip, bind_port))
+        server.listen(5)  # max backlog of connections
+        demisto.debug(f'Long Running: Listening on {bind_ip}:{bind_port}')
+        while True:
+            client_socket, address = server.accept()
+            demisto.debug(f'Long Running: Accepted connection from {address[0]}:{address[1]}')
+            client_handler = threading.Thread(
+                target=handle_client_connection,
+                args=(client_socket,)
+            )
+            client_handler.start()
+            demisto.updateModuleHealth(f'Context: {json.dumps(demisto.getIntegrationContext())}')
+    except Exception as e:
+        demisto.error(f'Long Running error: {str(e)}')
 
 def main():
     """
@@ -52,7 +109,7 @@ def main():
 
     proxy = demisto.params().get('proxy', False)
 
-    (f'Command being called is {demisto.command()}')
+    demisto.debug(f'Command being called is {demisto.command()}')
     try:
         client = Client(
             base_url=base_url,
