6fb9b627473b061968a2a120ac59179ca7bb73f3
Luigi Mori <lmori@paloaltonetworks.com>
2020-12-11 18:28:57 +0100

    Adds proxy support

--- Packs/X509Certificate/Scripts/CertificateFromSSLServer/CertificateFromSSLServer.py
+++ Packs/X509Certificate/Scripts/CertificateFromSSLServer/CertificateFromSSLServer.py
@@ -3,16 +3,17 @@ from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-impor
 from CommonServerUserPython import *  # noqa # pylint: disable=unused-wildcard-import
 
 
+import base64
+import http.client
+import os
 import urllib.parse
 import re
 import socket
-from collections import namedtuple
 import OpenSSL.SSL
 import OpenSSL.crypto
 from typing import (
     Dict, Any, Optional,
-    Tuple, Iterator, List,
-    TypedDict
+    Tuple, List, TypedDict
 )
 from operator import itemgetter
 
@@ -46,15 +47,19 @@ SSLCertificate = TypedDict('SSLCertificate', {
 ''' STANDALONE FUNCTION '''
 
 
-def verify_cb(conn, cert, errnum, depth, ok):
+def verify_cb(_conn, _cert, _errnum, _depth, ok):
     return ok
 
 
-def get_hostname_and_port(address: str) -> Tuple[str, int]:
+def get_hostname_and_port(address: str, force_http: bool = False) -> Tuple[str, int, Optional[str], Optional[str]]:
     normalized_address = address
-    if re.match(r'^[a-zA-Z]+:\/\/.*', address) is None:
+    scheme = re.match(r'^([a-zA-Z]+):\/\/.*', address)
+    if scheme is None:
         normalized_address = 'fake://' + address
-    parsed_url = urllib.parse.urlparse(normalized_address)
+    elif scheme.group(1).lower() != 'http' and force_http:
+        raise ValueError('Invalid URL scheme. Scheme for {address} should be "http".')
+
+    parsed_url: urllib.parse.ParseResult = urllib.parse.urlparse(normalized_address)
 
     if (hostname := parsed_url.hostname) is None:
         raise ValueError('Invalid address format.')
@@ -68,7 +73,7 @@ def get_hostname_and_port(address: str) -> Tuple[str, int]:
 
         port = SCHEME_TO_DEFAULT_PORT[parsed_url.scheme]
 
-    return hostname, port
+    return hostname, port, parsed_url.username, parsed_url.password
 
 
 def to_ssl_certificate(certificate: OpenSSL.crypto.X509) -> SSLCertificate:
@@ -81,10 +86,41 @@ def to_ssl_certificate(certificate: OpenSSL.crypto.X509) -> SSLCertificate:
     }
 
 
-def get_certificates(hostname: str, port: int, sni: Optional[str],
-                     protocol: Any, full_chain: bool) -> Optional[List[SSLCertificate]]:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((hostname, port))
+def proxy_tunnel(proxy: Tuple[str, int, Optional[str], Optional[str]], hostname: str, port: int, ) -> socket.SocketType:
+    http_conn = http.client.HTTPConnection(host=proxy[0], port=proxy[1])
+    http_conn.request("CONNECT", f'{hostname}:{port}', headers={"Host": f'{hostname}:{port}'})
+    response: http.client.HTTPResponse = http_conn.getresponse()
+    if response.status == http.client.PROXY_AUTHENTICATION_REQUIRED:
+        if proxy[2] is None or proxy[3] is None:
+            raise RuntimeError('Proxy authentication required but no credentials supplied.')
+
+        auth_basic = base64.b64encode(f'{proxy[2]}:{proxy[3]}'.encode('utf-8')).decode('ascii')
+        http_conn.request("CONNECT", f'{hostname}:{port}', headers={
+            "Host": f'{hostname}:{port}',
+            "Proxy-Authorization": f'basic {auth_basic}'
+        })
+        response = http_conn.getresponse()
+
+    if response.status != http.client.OK:
+        raise RuntimeError(f'Error response from proxy: {response.status} {response.reason}.')
+
+    return http_conn.sock
+
+
+def get_certificates(hostname: str,
+                     port: int,
+                     sni: Optional[str],
+                     protocol: Any,
+                     full_chain: bool,
+                     proxy: Optional[Tuple[str,
+                                           int,
+                                           Optional[str],
+                                           Optional[str]]]) -> Optional[List[SSLCertificate]]:
+    if proxy is not None:
+        sock = proxy_tunnel(proxy, hostname, port)
+    else:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((hostname, port))
 
     context = OpenSSL.SSL.Context(method=protocol)
     context.set_verify(OpenSSL.SSL.VERIFY_NONE, callback=verify_cb)
@@ -109,6 +145,9 @@ def get_certificates(hostname: str, port: int, sni: Optional[str],
                 ssl_certificate = to_ssl_certificate(c)
                 result[ssl_certificate['sha256']] = ssl_certificate
 
+    connection.shutdown()
+    connection.close()
+
     return sorted(list(result.values()), key=itemgetter('subject'))
 
 
@@ -120,7 +159,9 @@ def certificate_from_ssl_server_command(args: Dict[str, Any]) -> CommandResults:
     if address is None:
         raise ValueError('address argument is required.')
 
-    hostname, port = get_hostname_and_port(address)
+    hostname, port, username, password = get_hostname_and_port(address)
+    if username is not None or password is not None:
+        demisto.log('Username and password in address are ignored')
 
     arg_sni: str = args.get('sni', 'true')
     sni: Optional[str]
@@ -128,6 +169,15 @@ def certificate_from_ssl_server_command(args: Dict[str, Any]) -> CommandResults:
         sni = hostname if argToBoolean(arg_sni) else None
     except ValueError:
         sni = arg_sni
+
+    arg_proxy: str = args.get('proxy', 'false')
+    proxy: Optional[Tuple[str, int, Optional[str], Optional[str]]] = None
+    try:
+        if argToBoolean(arg_proxy):
+            https_proxy_env = os.environ.get('HTTPS_PROXY')
+            proxy = get_hostname_and_port(https_proxy_env, force_http=True) if https_proxy_env else None
+    except ValueError:
+        proxy = get_hostname_and_port(arg_proxy, force_http=True)
 
     arg_protocols = argToList(args.get('protocols', 'flex'))
     protocols = []
@@ -143,7 +193,7 @@ def certificate_from_ssl_server_command(args: Dict[str, Any]) -> CommandResults:
 
     certificates: Optional[List[SSLCertificate]]
     for proto in protocols:
-        certificates = get_certificates(hostname, port, sni, proto, full_chain)
+        certificates = get_certificates(hostname, port, sni, proto, full_chain, proxy)
         if certificates is not None:
             break
     else:
@@ -161,14 +211,15 @@ def certificate_from_ssl_server_command(args: Dict[str, Any]) -> CommandResults:
                 DBotScoreType.CERTIFICATE,
                 'X509Certificate',
                 Common.DBotScore.NONE
-        ))
+            ))
         for c in certificates
     ]
     readable_ouput = tableToMarkdown('Certificates', certificates, headers=['subject', 'issuer', 'sha256'])
 
     return CommandResults(
         readable_output=readable_ouput,
-        indicators=indicators
+        indicators=indicators,
+        ignore_auto_extract=True
     )
 
 
@@ -176,6 +227,8 @@ def certificate_from_ssl_server_command(args: Dict[str, Any]) -> CommandResults:
 
 
 def main():
+    import os
+    demisto.debug(f'{os.environ!r}')
     try:
         return_results(certificate_from_ssl_server_command(demisto.args()))
     except Exception as ex:
--- Packs/X509Certificate/Scripts/CertificateFromSSLServer/CertificateFromSSLServer_test.py
+++ Packs/X509Certificate/Scripts/CertificateFromSSLServer/CertificateFromSSLServer_test.py
@@ -1,62 +1,34 @@
-"""HelloWorld Script for Cortex XSOAR - Unit Tests file
-
-This file contains the Unit Tests for the HelloWorld Script based
-on pytest. Cortex XSOAR contribution requirements mandate that every
-script should have a proper set of unit tests to automatically
-verify that the integration is behaving as expected during CI/CD pipeline.
-
-Test Execution
---------------
-
-Unit tests can be checked in 3 ways:
-- Using the command `lint` of demisto-sdk. The command will build a dedicated
-  docker instance for your integration locally and use the docker instance to
-  execute your tests in a dedicated docker instance.
-- From the command line using `pytest -v` or `pytest -vv`
-- From PyCharm
-
-Example with demisto-sdk (from the content root directory):
-demisto-sdk lint -d Packs/HelloWorld/Scripts/HelloWorldScript
-
-Coverage
---------
-
-There should be at least one unit test per  function. In each unit
-test, the target command function is executed with specific parameters and the
-output of the command function is checked against an expected output.
-
-NOTE: we do not have to import or build a requests-mock instance explicitly.
-requests-mock library uses a pytest specific mechanism to provide a
-requests_mock instance to any function with an argument named requests_mock.
-
-More Details
-------------
-
-More information about Unit Tests in Cortex XSOAR:
-https://xsoar.pan.dev/docs/integrations/unit-testing
-
-Also please check the HelloWorld Integration Unit Tests file.
-
-"""
-
-from HelloWorldScript import say_hello, say_hello_command
+import pytest
 
 
-def test_say_hello():
-    result = say_hello('Dbot')
+def load_json(path):
+    import json
 
-    assert result == 'Hello Dbot'
+    with open(path, 'r') as f:
+        return json.load(f)
 
 
-def test_say_hello_command():
-    args = {
-        'name': 'Dbot'
-    }
+def test_get_hostname_and_port():
+    from CertificateFromSSLServer import get_hostname_and_port
 
-    response = say_hello_command(args)
+    test_vector = load_json('test_data/get_hostname_and_port.json')
+    for test in test_vector:
+        if test[1] is None:
+            with pytest.raises(ValueError):
+                get_hostname_and_port(test[0])
+            continue
 
-    assert response.outputs == {
-        'HelloWorld': {
-            'hello': 'Hello Dbot'
-        }
-    }
+        assert test[1] == list(get_hostname_and_port(test[0]))
+
+
+def test_get_hostname_and_port_http():
+    from CertificateFromSSLServer import get_hostname_and_port
+
+    test_vector = load_json('test_data/get_hostname_and_port_http.json')
+    for test in test_vector:
+        if test[1] is None:
+            with pytest.raises(ValueError):
+                get_hostname_and_port(test[0], force_http=True)
+            continue
+
+        assert test[1] == list(get_hostname_and_port(test[0]))
